589bf61e647d8032ce57833d2b69de2fb7af3463
odoodevelopmentinbangla <odoodevelopmentinbangla@gmail.com>
2025-11-16 15:36:03 +0600

    Add files via upload

---
+++ 01_rak_school_management/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'School Management',
+    'version': '1.0',
+    'category': 'tools',
+    'summary': '',
+    'description': """""",
+    'depends': [],
+    'data': [
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
+
